7e5aef5f286edebdd86c75c50240d86897eb6a04
Manuk <123746895+manuk2000@users.noreply.github.com>
2023-04-01 14:04:36 +0400

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
-
-import javax.lang.model.element.Element;
 
 public class Vector<E> {
 
